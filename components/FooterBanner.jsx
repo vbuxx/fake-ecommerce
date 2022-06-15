8c0978f2653c741d568f6bf87d28102f1bcd28f2
vbuxx <andhikapramana101@gmail.com>
2022-06-15 15:11:39 +0700

    Modify Footer & FooterBanner

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -22,7 +22,8 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
                     </Link>
                 </div>
 
-                <img src={urlFor(image)} className='footer-banner-image' />
+                <img src={urlFor(image)}
+                    className='footer-banner-image' />
             </div>
         </div>
     )
